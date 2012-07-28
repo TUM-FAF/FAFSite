a90d24fda9b70164bbfb405d67416384a95e2141
ana-balica <ana.balica@gmail.com>
2012-07-28 19:38:02 +0300

    update db

--- academics/admin.py
+++ academics/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from academics.models import Professor, Student, Alumni, Course
 
 class ProfessorAdmin(admin.ModelAdmin):
-	list_display = ('name', 'surname')
-	search_fields = ('name',)
+	list_display = ('name', 'surname', 'rank')
+	search_fields = ('name', 'surname',)
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ('name', 'surname')
-	search_fields = ('name',)
+	list_display = ('name', 'surname', 'group')
+	search_fields = ('name', 'surname',)
 
 class AlumniAdmin(admin.ModelAdmin):
 	list_display = ('name', 'surname')
