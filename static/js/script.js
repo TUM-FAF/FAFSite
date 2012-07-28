1240e4a199a2139cc21ead5690eb3e22f7515d9e
ana-balica <ana.balica@gmail.com>
2012-07-28 23:43:14 +0300

    People V.1.3

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,40 @@
 $(function() {
-	$('ul.side_nav>ul.subtitel').filter(':nth-child(n+3)').hide()
-	
-	$('ul.side_nav').on('click', 'li.title', function() {
-		console.log( $(this).next() );
-		$(this)
-			.next()
-			.slideDown('slow')
-			.siblings('ul')
-				.slideUp('slow');
-	});
+
+	sliding();
+	showHide();
+
+	// slide up and down items of the sidebar
+	function sliding() {
+		$('ul.side_nav>ul.subtitel').filter(':nth-child(n+3)').hide()
+		
+		$('ul.side_nav').on('click', 'li.title', function() {
+			console.log( $(this).next() );
+			$(this)
+				.next()
+				.slideDown('slow')
+				.siblings('ul')
+					.slideUp('slow');
+		});
+	}
+
+	// show and hide ID card of a person
+	function showHide() {
+		$('tr.id_card').hide();
+
+		$('tbody').on('click', 'tr.person', function() {
+			$(this)
+			 	.hide()
+				 	.next()
+				 	.fadeIn('slow');
+		});
+
+		$('tbody').on('click', 'a.hide_btn', function() {
+			$(this)
+				.parents('tr.id_card')
+				.hide()
+					.prev()
+					.show();
+
+		});
+	}
 }); 
