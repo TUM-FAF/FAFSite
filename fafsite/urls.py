31b5cb3f817e502c3ef7100879f1b8087e87254f
ana-balica <ana.balica@gmail.com>
2012-08-08 02:07:28 +0300

    Changed URL to Act and Ach

--- fafsite/urls.py
+++ fafsite/urls.py
@@ -11,28 +11,29 @@ urlpatterns = patterns('',
 	url(r'^$', index),
 	url(r'^index/$', index),
 	url(r'^about/$', about),
+	url(r'^admission/$', admission),
+	url(r'^achievements/$', achievements),
+	url(r'^activities/$', activities),
 	url(r'^courses/Calculus-1/$', about_course),
 	url(r'^courses/$', courses),
-    url(r'^timeline/$', timeline),
-	url(r'^admission/$', admission),
-    url(r'^people/$', people),
-    url(r'^tinymce/', include('tinymce.urls')),
-    # Examples:
-    # url(r'^$', 'fafsite.views.home', name='home'),
-    # url(r'^fafsite/', include('fafsite.foo.urls')),
+	url(r'^people/$', people),
+	url(r'^tinymce/', include('tinymce.urls')),
+	# Examples:
+	# url(r'^$', 'fafsite.views.home', name='home'),
+	# url(r'^fafsite/', include('fafsite.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	url(r'^grappelli/', include('grappelli.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 )
 
 
 if settings.DEBUG:
-    urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
+	urlpatterns = patterns('',
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+		{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+	url(r'', include('django.contrib.staticfiles.urls')),
 ) + urlpatterns
