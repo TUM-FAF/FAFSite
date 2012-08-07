9deef5fe72704f0554d681a786e80213ba5b39be
bumbu <bmbalex@gmail.com>
2012-08-07 17:24:00 +0300

    clearing up

--- academics/admin.py
+++ academics/admin.py
@@ -12,27 +12,6 @@ class UserMetaAdmin(admin.ModelAdmin):
 	list_display = ('user', 'meta', 'value')
 
 
-# class ProfessorAdmin(admin.ModelAdmin):
-# 	list_display = ('name', 'surname', 'rank')
-# 	search_fields = ('name', 'surname',)
-
-# class StudentAdmin(admin.ModelAdmin):
-# 	list_display = ('name', 'surname', 'group')
-# 	search_fields = ('name', 'surname',)
-
-# class AlumniAdmin(admin.ModelAdmin):
-# 	list_display = ('name', 'surname', 'group')
-# 	search_fields = ('name', 'surname',)
-
-# class CourseAdmin(admin.ModelAdmin):
-# 	list_display = ('subject_en', 'subject_ro', 'semester')
-# 	search_fields = ('semester',)
-
 admin.site.register(User, UserAdmin)
 admin.site.register(UserMetaKey, UserMetaKeyAdmin)
 admin.site.register(UserMeta, UserMetaAdmin)
-
-# admin.site.register(Professor, ProfessorAdmin)
-# admin.site.register(Student, StudentAdmin)
-# admin.site.register(Alumni, AlumniAdmin)
-# admin.site.register(Course, CourseAdmin)
