22935f3a7a3784a19ba30fda7e7d5a2e79739259
ana-balica <ana-balica@github.com>
2012-09-12 23:43:46 +0300

    database engine changed from sqlite to mysql

--- fafsite/settings.py
+++ fafsite/settings.py
@@ -16,10 +16,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join( PROJECT_PATH, 'db', 'fafdb.db' ).replace( '\\', '/' ),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': os.path.join( PROJECT_PATH, 'db', 'fafdb.db' ).replace( '\\', '/' ),                      # Or path to database file if using sqlite3.
+        'NAME': 'fafdb',
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'dev',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
