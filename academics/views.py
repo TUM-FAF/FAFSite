6e4ae7da1e457e886d3ba08f6b004c0303162780
ana-balica <ana-balica@github.com>
2012-11-07 20:56:07 +0200

    update academics views

--- academics/views.py
+++ academics/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, render_to_response
+from django.http import Http404
 from django.template import RequestContext
 from academics.models import *
 
@@ -36,9 +37,18 @@ def about_course(request, course):
 	except:
 		raise Http404()
 	return render(request, "about-course.html", 
-			{"activepage": "Courses", "menu": menu_items, "courses": courses, "course":course_info})
+			{"activepage": "Courses", "menu": menu_items, "courses": courses, "Course":course_info})
 
 # Main page of people
+# Ge a list of all distinct groups
+def get_groups():
+	group_dict = User.objects.values('group').distinct()
+	groups = []
+	for group in group_dict:
+		groups.append(group['group'])
+	groups = sorted(groups, reverse=True)
+	return groups
+
 # Lists all the professors and their attributes
 def professors(request):
 	global menu_items
@@ -46,23 +56,38 @@ def professors(request):
 	professors = UserMeta.objects.filter(value='professor')
 	for professor in professors:
 		userID = professor.user_id
-		# prof = User.objects.get(id=userID)
 		prof = UserExtended(userID)
 		courses = Course.objects.filter(professors=prof)
 		academics.append([prof, courses])
+	groups = get_groups()
+	student_groups = groups[:4]
+	alumni_groups = groups[4:]
 	return render(request, "professors.html", 
-		{"activepage": "People", "menu": menu_items, "academics": academics})
+		{"activepage": "People", "menu": menu_items, "academics": academics, 
+		'student_groups': student_groups, 'alumni_groups': alumni_groups})
 
 # Lists all the students filtered by the group with all their attributes
 def students(request, group):
 	global menu_items
+	students = []
+	student_metas = UserMeta.objects.filter(value='student')
+	for student in student_metas:
+		userID = student_metas.user_id
+		st = UserExtended(userID)
 
+	groups = get_groups()
+	student_groups = groups[:4]
+	alumni_groups = groups[4:]
 	return render(request, "students.html", 
-		{"activepage": "People", "menu": menu_items})
+		{"activepage": "People", "menu": menu_items,
+		'student_groups': student_groups, 'alumni_groups': alumni_groups})
 
 # Lists all the alumni filtered by the group with all their attributes
 def alumni(request, group):
 	global menu_items
-
+	groups = get_groups()
+	student_groups = groups[:4]
+	alumni_groups = groups[4:]
 	return render(request, "alumni.html", 
-		{"activepage": "People", "menu": menu_items})
+		{"activepage": "People", "menu": menu_items,
+		'student_groups': student_groups, 'alumni_groups': alumni_groups})
