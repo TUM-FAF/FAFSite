3bba3c3f6b3db19f05be78809edfed5a7b94f4f5
ana-balica <ana.balica@gmail.com>
2013-08-02 19:08:59 +0300

    handle POST profile form

--- fafsite/fafside/views.py
+++ fafsite/fafside/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from academics.models import UserExtended, User
 from .forms import Profile
@@ -10,12 +11,20 @@ def fafside_index(request):
     return render(request, 'fafside/main.html')
 
 
+@csrf_exempt
 @login_required(login_url='/login/')
 def profile(request):
     email = request.user.email
     user = User.objects.get(email=email)
     user_extended = UserExtended(user.id)
     form = Profile()
-    for field in form.fields:
-        form.fields.get(field).initial = getattr(user_extended, field)
+    if request.method == 'POST':
+        form = Profile(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            for field in cd:
+                setattr(user_extended, field, cd.get(field))
+    else:
+        for field in form.fields:
+            form.fields.get(field).initial = getattr(user_extended, field)
     return render(request, 'fafside/profile.html', {"form": form})
