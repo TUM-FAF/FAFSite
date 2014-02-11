c680b7eba02ab29abfea4a42123047f786132797
bumbu <bmbalex@gmail.com>
2014-02-11 13:42:38 +0200

    move media folder one level up

--- fafsite/fafsite/staging_settings.py
+++ fafsite/fafsite/staging_settings.py
@@ -54,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, "media").replace('\\', '/')
+MEDIA_ROOT = os.path.join(PROJECT_PATH, "..", "media").replace('\\', '/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
